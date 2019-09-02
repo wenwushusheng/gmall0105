639871ee15710771b924928faaa15f357e3867b7
laozhang <133019054@qq.com>
2019-09-02 21:36:51 +0800

    2019/9/2

---
+++ lianxiti/src/com/laozhang/xinjian.java
@@ -0,0 +1,13 @@
+package com.laozhang;
+
+/**
+ * @author zhangchunyu
+ * @create 2019-09-02 21:28
+ */
+public class xinjian {
+
+
+    public static void main(String[] args) {
+        System.out.println("我是新建的文件");
+    }
+}
